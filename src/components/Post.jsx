8a8b991a1d7406c8a95300abe92f80f38048e7cf
Gustavo Henrique <gusgegomes@hotmail.com>
2022-12-27 12:02:40 -0300

    meu primeiro commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import { Article } from 'phosphor-react';
+import styles from './Post.module.css';
+
+export function Post() {
+    return(
+       <article className={styles.post}>
+         <header>
+           <div className={styles.author}></div>
+         </header>
+       </article>
+    )
+}
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,23 @@
+import { PencilLine } from 'phosphor-react'
+import styles from "./sidebar.module.css"
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+          <img className={styles.cover} src="https://images.unsplash.com/photo-1533418264835-9871c7c2dbf0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZvcm11bGElMjAxfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=80https://images.unsplash.com/photo-1537402792645-b6d9a3ac3fad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9ybXVsYSUyMDF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+          
+          <div className={styles.profile}>
+            <img className={styles.avatar} src="http://github.com/gustavo-gomess.png" />
+
+            <strong>Gustavo Henrique</strong>
+            <span>Web Developer</span>
+          </div>
+
+          <footer>
+            <a href="#">
+              <PencilLine />
+              Editar seu perfil</a>
+        </footer>
+        </aside>
+    );
+}
